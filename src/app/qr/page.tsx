bdf0972fb5a91843ff9db9209b77e9100f7708c3
Nirvaan Salian <nirvaansaalian@gmail.com>
2025-09-17 10:22:25 +0530

    fix build

--- src/app/qr/page.tsx
+++
@@ -1,9 +0,0 @@
-import QRcode from "~/components/QRcode";
-
-export default function ShowQR() {
-  return (
-    <div className="flex min-h-[calc(100vh-4rem)] items-center justify-center">
-      <QRcode data={"https://flavorflow-ai.vercel.app"} />
-    </div>
-  );
-}
